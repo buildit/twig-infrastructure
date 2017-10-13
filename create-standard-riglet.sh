50ac9f05753f06dfa47989ff33533bc4a5ab224d
Mike Thomas <mathomas@users.noreply.github.com>
2017-10-13 13:20:05 -0600

    DB (Couch) layer added.  Seems to work!  More work to do, though. Twig-API configured for DB layer. Internal hosted zone DNS name changed (fixed, really:  prior "internal." name no-workie, I believe due to clashes). Create/delete uber-scripts improved (I think?).

--- create-standard-riglet.sh
+++ create-standard-riglet.sh
@@ -5,6 +5,12 @@ NC='\033[0m' # No Color
 
 BRANCH=${1:-master}
 
+function createEnvironment() {
+  echo "Y" | make create-foundation ENV=${1} && \
+  echo "Y" | make create-compute ENV=${1} && \
+  echo "Y" | make create-db ENV=${1}
+}
+
 cat .make
 
 printf  "\n${YELLOW}This command creates a full riglet based on the environment described above.${NC}\n"
@@ -12,12 +18,17 @@ read -p "Are you sure you want to proceed?  " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Y" | make create-foundation ENV=integration && \
-  echo "Y" | make create-compute ENV=integration && \
-  echo "Y" | make create-foundation ENV=staging && \
-  echo "Y" | make create-compute ENV=staging && \
-  echo "Y" | make create-foundation ENV=production && \
-  echo "Y" | make create-compute ENV=production && \
-  echo "Y" | make create-build REPO=twig-api REPO_BRANCH=${BRANCH} CONTAINER_PORT=3000 HEALTH_CHECK_PATH='/ping' LISTENER_RULE_PRIORITY=100 && \
-  echo "Y" | make create-build REPO=twig REPO_BRANCH=${BRANCH} CONTAINER_PORT=80 HEALTH_CHECK_PATH='/' LISTENER_RULE_PRIORITY=200
+  createEnvironment integration && \
+  createEnvironment staging && \
+  createEnvironment production && \
+  echo "Y" | make create-build REPO=twig-api \
+                    REPO_BRANCH=${BRANCH} \
+                    CONTAINER_PORT=3000 \
+                    HEALTH_CHECK_PATH='/ping' \
+                    LISTENER_RULE_PRIORITY=100 && \
+  echo "Y" | make create-build REPO=twig \
+                    REPO_BRANCH=${BRANCH} \
+                    CONTAINER_PORT=80 \
+                    HEALTH_CHECK_PATH='/' \
+                    LISTENER_RULE_PRIORITY=200
 fi
