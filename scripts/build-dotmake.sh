25fd393639ef20f5599ded74bbd6aca2f2fcd24f
Mike Thomas <mathomas@users.noreply.github.com>
2017-10-17 14:49:59 -0600

    Externalize certificate ARN (which had a region reference in it).

--- scripts/build-dotmake.sh
+++ scripts/build-dotmake.sh
@@ -9,6 +9,7 @@ read -p 'AWS Profile: ' profile
 read -p 'Project: ' project
 read -p 'Repository OAuth token: ' repo_token
 read -p 'AWS region: ' region
+read -p 'AWS Certificate ARN for domain: ' cert_arn
 echo
 
 cat << EOF > .make
@@ -19,6 +20,7 @@ PROFILE = ${profile}
 PROJECT = ${project}
 REPO_TOKEN = ${repo_token}
 REGION = ${region}
+CERT_ARN = ${cert_arn}
 EOF
 
 echo 'Saved .make!'
